7b9471d994b635ac7bd70e4a72f476edb0476691
Isaac yanez <isaac231467@gmail.com>
2021-12-24 15:31:08 -0600

    updated template

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer')
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 
 const promptUser = () => {
@@ -134,13 +136,11 @@ return inquirer
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
-  // will be uncommented in lesson 4
-  // const pageHTML = generatePage(portfolioData);
-  // fs.writeFile('./index.html', pageHTML, err => {
-  //   if (err) throw new Error(err);
-  //   console.log('Page created! Check out index.html in this directory to see it!');
-  // });
+  const pageHTML = generatePage(portfolioData);
+  fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+    console.log('Page created! Check out index.html in this directory to see it!');
+  });
 });
 
 
